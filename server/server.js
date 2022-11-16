22a4ac5dc3d5e374cd58203bc65b1fa45ea58d7a
Edward Karlsson <edwkar@student.chalmers.se>
2022-11-16 17:32:36 +0100

    Created remaining rest api methods POST, DELETE, PUT

--- server/server.js
+++ server/server.js
@@ -16,11 +16,38 @@ mongoose
   .catch(console.error)
 
 const Todo = require("./models/Todo")
+const { restart } = require("nodemon")
 
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find()
 
   res.json(todos)
+})
+
+app.post("/todos/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  })
+
+  todo.save()
+
+  res.json(todo)
+})
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id)
+
+  res.json(result)
+})
+
+app.put("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id)
+
+  todo.complete = !todo.complete
+
+  todo.save()
+
+  res.json(todo)
 })
 
 app.listen(3001, () => console.log("Server Started On Port 3001"))
